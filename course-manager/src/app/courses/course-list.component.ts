dd66e36d7df1ab2a27905ca55092cb76b0a7f180
Eder Vieira <eder.edy@gmail.com>
2022-07-22 23:32:19 -0400

    Angular components and changes

--- course-manager/src/app/courses/course-list.component.ts
+++ course-manager/src/app/courses/course-list.component.ts
@@ -1,9 +1,37 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Course } from "./course";
 
 @Component({
     selector: 'app-course-list',
-    template: '<h2>Course List</h2>'
+    templateUrl: './course-list.component.html'
 })
-export class CourseListComponent{
+export class CourseListComponent implements OnInit{
+
+    courses: Course[] = []; 
+
+    ngOnInit(): void{
+        this.courses = [
+            {
+                id: 1,
+                name: 'Angular: Forms',
+                price: 99.99,
+                imageUrl: '/assets/images/forms.png',
+                code: 'XPS+1',
+                duration: 120,
+                rating: 4.5,
+                realeseDate: 'August, 22, 2022'
+            },
+            {
+                id: 1,
+                name: 'Angular: HTTP',
+                price: 99.99,
+                imageUrl: '/assets/images/http.png',
+                code: 'XPS+2',
+                duration: 112,
+                rating: 4,
+                realeseDate: 'October, 10, 2022'
+            }
+        ]
+    }
 
 }
---
+++ course-manager/src/app/courses/course.ts
@@ -0,0 +1,12 @@
+
+
+export class Course{
+    id: number = 0;
+    name: string = '';
+    price: number = 0;
+    imageUrl: string = '';
+    code: string = '';
+    duration: number = 0;
+    rating: number = 0;
+    realeseDate: string = '';
+}
