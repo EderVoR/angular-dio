80af4ab071ea476b7ff7327ab4466cc1a6df7146
Eder Vieira <eder.edy@gmail.com>
2022-07-24 22:24:00 -0400

    Rotas

--- course-manager/src/app/courses/course-list.component.ts
+++ course-manager/src/app/courses/course-list.component.ts
@@ -4,7 +4,6 @@ import { Course, } from "./course";
 import { CourseService } from "./couser.service";
 
 @Component({
-    selector: 'app-course-list',
     templateUrl: './course-list.component.html'
 })
 export class CourseListComponent implements OnInit{
