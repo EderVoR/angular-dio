be26a19c11a0e139335501a14535bcfb08142e1d
Eder Vieira <eder.edy@gmail.com>
2022-07-27 22:58:03 -0400

    template e formulario

--- course-manager/src/app/courses/course-info.component.ts
+++ course-manager/src/app/courses/course-info.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { Course } from "./course";
+import { CourseService } from "./couser.service";
 
 @Component({
     templateUrl: 'course-info.component.html'
 })
 export class CourseInfoComponent implements OnInit{
 
-    courseId: number = 1;
+    course: Course;
 
-    constructor(private activatedRoute: ActivatedRoute){
+    constructor(private activatedRoute: ActivatedRoute, private courseService: CourseService){
         
     }
     
     ngOnInit(): void {
-        this.courseId = +this.activatedRoute.snapshot.paramMap.get('id');
+        this.course = this.courseService.retrieveById(+this.activatedRoute.snapshot.paramMap.get('id'));
+    }
+
+    save(){
+        this.courseService.save(this.course);
     }
 }
