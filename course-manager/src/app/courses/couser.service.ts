8893fd627715efd71fa2049f75aaa07d3a3a11ee
Eder Vieira <eder.edy@gmail.com>
2022-07-27 23:52:44 -0400

    Requisição em HTTP

--- course-manager/src/app/courses/couser.service.ts
+++ course-manager/src/app/courses/couser.service.ts
@@ -1,26 +1,33 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 import { Course } from "./course";
 
 @Injectable({
     providedIn: 'root'
 })
 export class CourseService{
+
+    private courseUrl: string = 'http://localhost:3100/api/courses';
+
+    constructor(private httpCliente: HttpClient){
+
+    }
     
-    retrieveAll(): Course[] {
-        return COURSES;
+    retrieveAll(): Observable<Course[]> {
+        return this.httpCliente.get<Course[]>(this.courseUrl);
     }   
 
-    retrieveById(id: number) : Course {
-        return COURSES.find((courseIterator: Course) => courseIterator.id === id);
+    retrieveById(id: number) : Observable<Course> {
+        return this.httpCliente.get<Course>(`${this.courseUrl}/${id}`);
     }
 
-    save(course: Course) : void{
+    save(course: Course) : Observable<Course> {
         if(course.id){
-            const index = COURSES.findIndex((courseIterator: Course) => 
-                        courseIterator.id === course.id);
-            COURSES[index] = course;
+            return this.httpCliente.put<Course>(`${this.courseUrl}/${course.id}`, course);
+        } else{
+            return this.httpCliente.post<Course>(`${this.courseUrl}`, course);
         }
-
     }
 
 }
